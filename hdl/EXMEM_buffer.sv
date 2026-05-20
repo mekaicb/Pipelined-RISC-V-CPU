2def3901908778d3fd9fdb064b8c0a0c082f4c11
Mekai <root@DESKTOP-M2F8409.localdomain>
2026-05-20 18:57:03 -0400

    Removed jal control

--- hdl/EXMEM_buffer.sv
+++ hdl/EXMEM_buffer.sv
@@ -1,6 +1,6 @@
 module EXMEM_buffer(
 	input logic clk, rst_n,
-	input logic branch_i, memread_i, memwrite_i, regwrite_i, memtoreg_i, jal_i,
+	input logic branch_i, memread_i, memwrite_i, regwrite_i, memtoreg_i,
 	input logic [31:0] imm_i,
 	input logic zero,
 	input logic ALU_result_i,
@@ -19,8 +19,7 @@ module EXMEM_buffer(
 			memread_o <= 1'b0;
 			memwrite_o <= 1'b0;
 			regwrite_o <= 1'b0;
-			memtoreg_o <= 1'b0;
-			jal_o <= 1'b0;
+			memtoreg_o <= 2'b0;
 			imm_o <= 32'b0;
 			ALU_result_o <= 32'b0;
 			rs2_data_o <= 32'b0;
@@ -32,7 +31,6 @@ module EXMEM_buffer(
 			memwrite_o <= memwrite_i;
 			regwrite_o <= regwrite_i;
 			memtoreg_o <= memtoreg_i;
-			jal_o <= jal_i;
 			imm_o <= imm_i;
 			ALU_result_o <= ALU_result_i;
 			rs2_data_o <= rs2_data_i;
