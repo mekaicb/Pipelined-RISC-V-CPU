734b12bc67279c0d23e8b580f76d8b0cf5cf215d
Mekai <root@DESKTOP-M2F8409.localdomain>
2026-05-24 18:31:01 -0400

    Changed testing method

--- test/tb_CPU.sv
+++ test/tb_CPU.sv
@@ -5,8 +5,13 @@ module tb_CPU();
 	logic [6:0] opcode;
 	logic [2:0] funct3;
 	logic funct7;
+	logic hazard;
+	
 	int c_count;
+	
 	string inst_type;
+	string hazard_detected;
+	
 	
 	CPU dut(.clk(clk), .rst_n(rst_n));
 
@@ -19,14 +24,38 @@ module tb_CPU();
 	
 	always #5ns clk = ~clk; // 100MHz clk
 	
+	
+	
+	// Automatically create a file and load the instruction
+	task automatic test_instruction(
+		input logic [31:0] instr1, instr2
+	);
+		dut.ROM.mem_array[0] = instr1;
+		dut.ROM.mem_array[1] = instr2;
+		
+		rst_n = 0; 	 // Clear all pipeline registers from previous test
+		#11			 // prevent weird timing bugs if held for less than/exactly one clock cycle
+		rst_n = 1;
+		
+	endtask
+	
+	// Testing all 40 RV32I instructions. Using online assembler to convert RISCV to machine code
 	initial begin
-		#10;
-		rst_n <= 1;
+		
+		// ADDI
+		test_instruction(32'h00720193, 32'h0);
+		#50;
+		if(dut.reg_file.reg_out[3] == 32'h00000007)
+			$display("ADDI PASSED\n");
+		else
+			$display("ADDI FAILED\n");
 	end
 	
+	
+	// Block to determine instructions
 	always_comb begin
 		
-		opcode = dut.opcode;
+		opcode = dut.opcode; // hierarchical references
 		funct3 = dut.funct3;
 		funct7 = dut.funct7;
 		
@@ -101,16 +130,27 @@ module tb_CPU();
 			7'b0010111 : inst_type = "AUIPC";
 			
 			7'b1101111 : inst_type = "JAL";
+			
+			7'b0000000 : inst_type = "NOP";
 
 			default    : inst_type = "Unknown";
 		endcase
 	end
-
 	
 	always @(posedge clk) begin
 		c_count = c_count + 1;
-		$display("| Time: %0t | Cycle: %0d | Instruction Type: %s |", $time, c_count, inst_type);
+		
+		hazard = dut.IDEX_hazard_flush;
+		if(hazard) begin
+			hazard_detected = "YES";
+		end
+		else begin
+			hazard_detected = "NO";
+		end	
+		
+		$display("| Time: %0t | Cycle: %0d | Instruction Type: %s | Hazard?: %s |", $time, c_count, inst_type, hazard_detected);
 	end
+	
 	
 endmodule
 	
