06d2a744bfc1fd4af8d7c178e29dbd1fb30e9569
Mekai <root@DESKTOP-M2F8409.localdomain>
2026-05-23 20:05:56 -0400

    Added hazard detection unit

---
+++ hdl/hazard_detection.sv
@@ -0,0 +1,22 @@
+module hazard_detection(
+	input logic [4:0] IFID_rs1_addr, IFID_rs2_addr, IDEX_rd_addr,
+	input logic IDEX_memread,
+	output logic pcwrite, IFID_write, IDEX_flush
+);
+
+	always_comb begin
+		pcwrite = 1'b0;
+		IFID_write = 1'b0;
+		IDEX_flush = 1'b0;
+		
+		if(IDEX_memread) begin
+			if(IDEX_rd_addr == IFID_rs1_addr || IDEX_rd_addr == IFID_rs2_addr) begin
+				pcwrite = 1'b1; // Stall PC
+				IFID_write = 1'b1; // Stall IFID buffer
+				IDEX_flush = 1'b1;
+			end
+		end
+	end
+		
+
+endmodule
