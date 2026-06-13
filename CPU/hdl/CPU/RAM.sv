f0aefdf767f02908fe61dc93c38b85e4eb11d451
Mekai <mekai.bingham@gmail.com>
2026-06-13 18:51:43 -0400

    Fixed M9K inference

--- CPU/hdl/CPU/RAM.sv
+++ CPU/hdl/CPU/RAM.sv
@@ -6,11 +6,12 @@ module RAM(
 	output logic [31:0] data_o
 	);
 	
-	logic [2:0] byte_en; // Selects which byte of the word to load from memory, or store to memory
-	logic [1:0] byte_sel;
+	logic [2:0] byte_en, byte_en_r; // Selects which byte of the word to load from memory, or store to memory
+	logic [1:0] byte_sel, byte_sel_r;
+	logic [31:0] data_temp;
 	
 	logic [31:0] eff_addr;
-	(* ramstyle = "M9K" *) logic [31:0] mem_array[0:15359]; // Word addressible 15kB RAM. (15kB = 3840 words = 15360 bytes)
+	(* ramstyle = "M9K" *) logic [31:0] mem_array[0:3839]; // Word addressible 15kB RAM. (15kB = 3840 words = 15360 bytes)
 	
 	localparam offset = 32'h0000A000; // Start at 15kB
 	
@@ -24,76 +25,87 @@ module RAM(
 			3'b010 : byte_en = 3'b010; // LW/SW, select full word
 			3'b100 : byte_en = 3'b011; // LBU, select byte 0 & sign extend
 			3'b101 : byte_en = 3'b100; // LHU, select byte 0 & sign extend
-			default : byte_en = 2'b10;
+			default : byte_en = 3'b010;
 		endcase
 	end
 	
-	always_ff @(posedge clk) begin // Synchronous read
+		always_ff @(posedge clk) begin // Synchronous read
 		if(memread) begin 
-			case(byte_en) 
-				3'b000 : begin // Will be selecting one byte
-					case(byte_sel) // Which one byte to select specfically from the word in memory?
-						2'b00 : data_o <= {24'b0, mem_array[eff_addr[15:2]][7:0]}; // Select the first byte of the word from the memory array, zero extend
-						2'b01 : data_o <= {24'b0, mem_array[eff_addr[15:2]][15:8]}; 
-						2'b10 : data_o <= {24'b0, mem_array[eff_addr[15:2]][23:16]};
-						2'b11 : data_o <= {24'b0, mem_array[eff_addr[15:2]][31:24]};
-					endcase
-				end
-				3'b001 : begin // Select two bytes
-					case(byte_sel) // Which two bytes to select specifically
-						2'b00 : data_o <= {16'b0, mem_array[eff_addr[15:2]][15:0]};
-						2'b10 : data_o <= {16'b0, mem_array[eff_addr[15:2]][31:16]};
-					endcase
-				end
-				3'b010 : begin
-					data_o <= mem_array[eff_addr[15:2]];
-				end
-				3'b011 : begin
-					case(byte_sel)
-						2'b00 : data_o <= {{24{mem_array[eff_addr[15:2]][7]}}, mem_array[eff_addr[15:2]][7:0]}; // Sign extend the byte to 32 bits
-						2'b01 : data_o <= {{24{mem_array[eff_addr[15:2]][15]}}, mem_array[eff_addr[15:2]][15:8]}; // Select the 2nd byte, place as lowest byte and sign extend to 32 bits
-						2'b10 : data_o <= {{24{mem_array[eff_addr[15:2]][23]}}, mem_array[eff_addr[15:2]][23:16]};
-						2'b11 : data_o <= {{24{mem_array[eff_addr[15:2]][31]}}, mem_array[eff_addr[15:2]][31:24]};
-					endcase
-				end
-				3'b100 : begin
-					case(byte_sel)
-						2'b00 : data_o <= {{16{mem_array[eff_addr[15:2]][15]}}, mem_array[eff_addr[15:2]][15:0]}; // Sign extend the byte to 32 bits
-						2'b01 : data_o <= {{16{mem_array[eff_addr[15:2]][31]}}, mem_array[eff_addr[15:2]][31:16]}; // Select the 2nd byte, place as lowest byte and sign extend to 32 bits
-					endcase
-				end
-				default : data_o <= mem_array[eff_addr[15:2]]; // default read the full word
-			endcase
-		end
-		if(memwrite) begin
+			data_temp <= mem_array[eff_addr[13:2]]; // Simple read 
+			byte_sel_r <= byte_sel; // prevent being overwritten on the next cycle
+			byte_en_r <= byte_en;
+ 		end
+		if(memwrite) begin // Write can be complicated
 			case(byte_en)
 				2'b00 : begin
 					case(byte_sel)
-						2'b00 : mem_array[eff_addr[15:2]][7:0] <= data_i[7:0];
-						2'b01 : mem_array[eff_addr[15:2]][15:8] <= data_i[7:0];
-						2'b10 : mem_array[eff_addr[15:2]][23:16] <= data_i[7:0];
-						2'b11 : mem_array[eff_addr[15:2]][31:24] <= data_i[7:0];
+						2'b00 : mem_array[eff_addr[13:2]][7:0] <= data_i[7:0];
+						2'b01 : mem_array[eff_addr[13:2]][15:8] <= data_i[7:0];
+						2'b10 : mem_array[eff_addr[13:2]][23:16] <= data_i[7:0];
+						2'b11 : mem_array[eff_addr[13:2]][31:24] <= data_i[7:0];
 					endcase
 				end
 				2'b01 : begin
 					case(byte_sel)
-						2'b00 : mem_array[eff_addr[15:2]][15:0] <= data_i[15:0];
-						2'b01 : mem_array[eff_addr[15:2]][31:16] <= data_i[15:0];
+						2'b00 : mem_array[eff_addr[13:2]][15:0] <= data_i[15:0];
+						2'b01 : mem_array[eff_addr[13:2]][31:16] <= data_i[15:0];
 					endcase
 				end
 				2'b10 : begin
-					mem_array[eff_addr[15:2]] <= data_i;
+					mem_array[eff_addr[13:2]] <= data_i;
 				end
-				default : mem_array[eff_addr[15:2]] <= data_i;
+				default : mem_array[eff_addr[13:2]] <= data_i;
 			endcase
-		end
-			
+		end	
 	end
 	
+	always_comb begin // Handles the slicing and extension of the word read from memory
+		case(byte_en_r)
+			3'b000 : begin // Will be selecting one byte
+				case(byte_sel_r) // Which one byte to select specfically from the word in memory?
+					2'b00 : data_o = {24'b0, data_temp[7:0]}; // Select the first byte of the word from the memory array, zero extend
+					2'b01 : data_o = {24'b0, data_temp[15:8]};
+					2'b10 : data_o = {24'b0, data_temp[23:16]};
+					2'b11 : data_o = {24'b0, data_temp[31:24]};
+				endcase
+			end
+			3'b001 : begin // Select two bytes
+				case(byte_sel_r) // Which two bytes to select specifically
+					2'b00 : data_o = {16'b0, data_temp[15:0]};
+					2'b10 : data_o = {16'b0, data_temp[31:16]};
+					default : data_o = {16'b0, data_temp[15:0]};
+				endcase
+			end
+			3'b010 : begin
+				data_o = data_temp;
+			end
+			3'b011 : begin
+				case(byte_sel_r)
+					2'b00 : data_o = {{24{data_temp[7]}}, data_temp[7:0]}; // Sign extend the byte to 32 bits
+					2'b01 : data_o = {{24{data_temp[15]}}, data_temp[15:8]}; // Select the 2nd byte, place as lowest byte and sign extend to 32 bits
+					2'b10 : data_o = {{24{data_temp[23]}}, data_temp[23:16]};
+					2'b11 : data_o = {{24{data_temp[31]}}, data_temp[31:24]};
+				endcase
+			end
+			3'b100 : begin
+				case(byte_sel_r)
+					2'b00 : data_o = {{16{data_temp[15]}}, data_temp[15:0]}; // Sign extend the halfword to 32 bits
+					2'b01 : data_o = {{16{data_temp[31]}}, data_temp[31:16]}; // Select the 2nd halfword, sign extend to 32 bits
+					default : data_o = {{16{data_temp[15]}}, data_temp[15:0]};
+				endcase
+			end
+			default : data_o = data_temp; // default read the full word
+		endcase
+	end
 endmodule
 	
 	
 /*
-	- [15:2] because we are assuming word aligned address inputs
+	- [13:2] because we are assuming word aligned address inputs (also log2(3840) = 11.9 address bits)
 	- Assuming the lowest two bits refer to the byte address
+	
+	- Ported ram can only do something simple like output <= input[addr]
+		- Something like output <= input[addr][7:0] becomes too complicated due to the slicing, and synthesizer infers flip flops rather than ram
+		- Solution is to do the slicing and extension asynchronously in a seperate block
 */
+
