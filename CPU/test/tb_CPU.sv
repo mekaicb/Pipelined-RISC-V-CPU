f7d449ba510ad93de4cecfe10013f91c9a12dd4c
Mekai <mekai.bingham@gmail.com>
2026-07-01 23:08:02 -0400

    Added way to calculate CPI

--- CPU/test/tb_CPU.sv
+++ CPU/test/tb_CPU.sv
@@ -2,17 +2,26 @@
 
 module tb_CPU();
 	logic clk, rst_n;
+	logic clk_25;
+	logic [3:0] VGA_R, VGA_G, VGA_B;
+	logic hsync, vsync;
+	logic [9:0] LED;
 	logic [6:0] opcode;
 	logic [2:0] funct3;
 	logic funct7;
 
 	int pass_count, sumtest_pass;
-	localparam RAM_BASE = 32'h10000;
 	string inst_type;
 	
-	CPU dut(.clk(clk), .rst_n(rst_n));
+	logic [63:0] cycle_count, finish_count;
+	logic measuring;
+
 	
-	always #5ns clk = ~clk; // 100MHz clk
+	CPU dut(.clk(clk), .rst_n(rst_n), .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B), .hsync(hsync), .vsync(vsync), .LED(LED));
+	
+	always #10ns clk = ~clk; // 50MHz 
+	
+	always #37.72ns clk_25 = ~clk_25; // 25MHz
 	
 	task automatic test_instruction(
 		input logic [31:0] instr1, instr2, instr3, instr4
@@ -23,30 +32,30 @@ module tb_CPU();
 		dut.ROM.mem_array[3] = instr4;
 		
 		rst_n = 0; 	 // Clear all pipeline registers from previous test
-		#20		 // prevent weird timing bugs if held for less than/exactly one clock cycle
+		#30		 // prevent weird timing bugs if held for less than/exactly one clock cycle
 		rst_n = 1;	
 	endtask
 	
 	task automatic clear_all();
 		rst_n = 0;
-		#20
+		#40
 		rst_n = 1;
-		for(int i = 0; i<32; i=i+1) begin
-			dut.RAM.mem_array[i] = 8'b0; // Clear memory (Assume only first 32 bytes in RAM used for testing)
+		
+		for(int i = 0; i<8; i=i+1) begin
+			dut.RAM.mem_array[i] = 32'b0; // Clear memory (Assume only first 8 words in RAM used for testing)
 		end
+		
 	endtask
 	
 	// Testing all 40 RV32I instructions. Using online assembler to convert RISCV to machine code
 	initial begin
 		clk <= 1;
-		rst_n <= 0;
 		pass_count = 0;
 		$timeformat(-9, 0, " ns");
-		
-		// LUI/ADDI/SW (li x4, 0x12345678 + sw x4, RAM_BASE(x0) = lui x4, 0x12345 + addi x4, 0x678 + lui x3, 0x00000010 + sw x4, 0(x3)) 
-		
-		test_instruction(32'h12345237, 32'h67820213, 32'h000101b7, 32'h0041A023);
-		#70
+
+		// LUI/ADDI/SW (li x4, 0x12345678 + sw x4, RAM_BASE(x0) = lui x4, 0x12345 + addi x4, 0x678 + lui x3, 0x1 + sw x4, 0x400(x3)) 
+		test_instruction(32'h12345237, 32'h67820213, 32'h000011B7, 32'h4041A023);
+		#200
 		if(dut.reg_file.reg_out[4] == 32'h12345678) begin
 			$display("LUI PASSED\nADDI PASSED");
 			pass_count = pass_count + 2;
@@ -55,7 +64,7 @@ module tb_CPU();
 			$display("LUI FAILED\nADDI FAILED\n");
 		end
 
-		if({dut.RAM.mem_array[3], dut.RAM.mem_array[2], dut.RAM.mem_array[1], dut.RAM.mem_array[0]} == 32'h12345678) begin
+		if({dut.RAM.mem_array[0]}== 32'h12345678) begin
 			$display("SW PASSED");
 			pass_count = pass_count + 1;
 		end
@@ -67,7 +76,7 @@ module tb_CPU();
 		
 		// ADD (addi x3, x0, 0x0000000A, addi x4, x0, 0x0000000B, add x3, x3, x4)
 		test_instruction(32'h00A00193, 32'h00B00213, 32'h004181B3, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[3] == 32'd21) begin
 			$display("ADD PASSED");
 			pass_count = pass_count + 1;
@@ -80,7 +89,7 @@ module tb_CPU();
 		
 		// SUB (addi x5, x0, 20, addi x6, x0, 8, sub x7, x5, x6)
 		test_instruction(32'h01400293, 32'h00800313, 32'h406283B3, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[7] == 32'd12) begin
 			$display("SUB PASSED");
 			pass_count = pass_count + 1;
@@ -93,7 +102,7 @@ module tb_CPU();
 
 		// AND (addi x5, x0, 15, addi x6, x0, 51, and x7, x5, x6)
 		test_instruction(32'h00F00293, 32'h03300313, 32'h0062F3B3, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[7] == 32'h03) begin
 			$display("AND PASSED");
 			pass_count = pass_count + 1;
@@ -106,7 +115,7 @@ module tb_CPU();
 
 		// OR (addi x5, x0, 15, addi x6, x0, 51, or x8, x5, x6)
 		test_instruction(32'h00F00293, 32'h03300313, 32'h0062E433, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[8] == 32'h3F) begin
 			$display("OR PASSED");
 			pass_count = pass_count + 1;
@@ -119,7 +128,7 @@ module tb_CPU();
 
 		// SLLI (addi x5, x0, 1, slli x6, x5, 4)
 		test_instruction(32'h00100293, 32'h00429313, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[6] == 32'd16) begin
 			$display("SLLI PASSED");
 			pass_count = pass_count + 1;
@@ -130,9 +139,9 @@ module tb_CPU();
 		
 		clear_all();
 
-		// LW (lui x3, 0x00010, addi x4, x0, 0x99, sw x4, 0(x3), lw x5, 0(x3))
-		test_instruction(32'h000101b7, 32'h09900213, 32'h0041a023, 32'h0001a283);
-		#80
+		// LW (lui x3, 0x1, addi x4, x0, 0x99, sw x4, 0x400(x3), lw x5, 0x400(x3))
+		test_instruction(32'h000011b7, 32'h09900213, 32'h4041a023, 32'h4001a283);
+		#220
 		if(dut.reg_file.reg_out[5] == 32'h99) begin
 			$display("LW PASSED");
 			pass_count = pass_count + 1;
@@ -145,7 +154,7 @@ module tb_CPU();
 		
 		// XOR (addi x5, x0, 15, addi x6, x0, 9, xor x7, x5, x6)
 		test_instruction(32'h00F00293, 32'h00900313, 32'h0062C3B3, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[7] == 32'd6) begin
 			$display("XOR PASSED");
 			pass_count = pass_count + 1;
@@ -158,7 +167,7 @@ module tb_CPU();
 
 		// SLT (addi x5, x0, 5, addi x6, x0, 10, slt x7, x5, x6)
 		test_instruction(32'h00500293, 32'h00A00313, 32'h0062A3B3, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[7] == 32'd1) begin
 			$display("SLT PASSED");
 			pass_count = pass_count + 1;
@@ -171,7 +180,7 @@ module tb_CPU();
 
 		// BEQ (addi x3, x0, 5, beq x3, x3, 0x00000008, addi x4, x0, 0x9, addi x5, x0, 0x1)
 		test_instruction(32'h00500193, 32'h00318463, 32'h00900213, 32'h00100293);
-		#100
+		#300
 		if(dut.reg_file.reg_out[4] != 32'd9 && dut.reg_file.reg_out[5] == 32'd1) begin
 			$display("BEQ PASSED");
 			pass_count = pass_count + 1;
@@ -184,7 +193,7 @@ module tb_CPU();
 
 		// BNE (addi x3, x0, 5, bne x3, x0, 0x00000008, addi x4, x0, 0x9, addi x5, x0, 0x1)
 		test_instruction(32'h00500193, 32'h00019463, 32'h00900213, 32'h00100293);
-		#100
+		#260
 		if(dut.reg_file.reg_out[4] != 32'd9 && dut.reg_file.reg_out[5] == 32'd1) begin
 			$display("BNE PASSED");
 			pass_count = pass_count + 1;
@@ -197,7 +206,7 @@ module tb_CPU();
 		
 		// SLTU (addi x3, x0, 5, addi x4, x0, 10, sltu x5, x3, x4)
 		test_instruction(32'h00500193, 32'h00A00213, 32'h0041B2B3, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[5] == 32'd1) begin
 			 $display("SLTU PASSED");
 			 pass_count = pass_count + 1;
@@ -211,7 +220,7 @@ module tb_CPU();
 
 		// SRLI (addi x3, x0, 16, srli x4, x3, 2)
 		test_instruction(32'h01000193, 32'h0021D213, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] == 32'd4) begin
 			 $display("SRLI PASSED");
 			 pass_count = pass_count + 1;
@@ -225,7 +234,7 @@ module tb_CPU();
 
 		// SRAI (addi x3, x0, -8, srai x4, x3, 1)
 		test_instruction(32'hFF800193, 32'h4011D213, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] == 32'hFFFFFFFC) begin
 			 $display("SRAI PASSED");
 			 pass_count = pass_count + 1;
@@ -238,7 +247,7 @@ module tb_CPU();
 		
 		// SLTI (addi x3, x0, 5, slti x4, x3, 10)
 		test_instruction(32'h00500193, 32'h00A1A213, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] == 32'd1) begin
 			 $display("SLTI PASSED");
 			 pass_count = pass_count + 1;
@@ -252,7 +261,7 @@ module tb_CPU();
 
 		// SLTIU (addi x3, x0, 5, sltiu x4, x3, 10)
 		test_instruction(32'h00500193, 32'h00A1B213, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] == 32'd1) begin
 			 $display("SLTIU PASSED");
 			 pass_count = pass_count + 1;
@@ -266,7 +275,7 @@ module tb_CPU();
 
 		// ANDI (addi x3, x0, 15, andi x4, x3, 9)
 		test_instruction(32'h00F00193, 32'h0091F213, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] == 32'd9) begin
 			 $display("ANDI PASSED");
 			 pass_count = pass_count + 1;
@@ -279,7 +288,7 @@ module tb_CPU();
 		
 		// XORI (addi x3, x0, 15, xori x4, x3, 9)
 		test_instruction(32'h00F00193, 32'h0091C213, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] == 32'd6) begin
 			 $display("XORI PASSED");
 			 pass_count = pass_count + 1;
@@ -292,7 +301,7 @@ module tb_CPU();
 
 		// ORI (addi x3, x0, 15, ori x4, x3, 16)
 		test_instruction(32'h00F00193, 32'h0101E213, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] == 32'd31) begin
 			 $display("ORI PASSED");
 			 pass_count = pass_count + 1;
@@ -305,7 +314,7 @@ module tb_CPU();
 
 		// SLL (addi x3, x0, 1, addi x4, x0, 4, sll x5, x3, x4)
 		test_instruction(32'h00100193, 32'h00400213, 32'h004192B3, 32'h0);
-		#80
+		#220
 		if(dut.reg_file.reg_out[5] == 32'd16) begin
 			 $display("SLL PASSED");
 			 pass_count = pass_count + 1;
@@ -318,7 +327,7 @@ module tb_CPU();
 
 		// SRL (addi x3, x0, 16, addi x4, x0, 2, srl x5, x3, x4)
 		test_instruction(32'h01000193, 32'h00200213, 32'h0041D2B3, 32'h0);
-		#80
+		#220
 		if(dut.reg_file.reg_out[5] == 32'd4) begin
 			 $display("SRL PASSED");
 			 pass_count = pass_count + 1;
@@ -331,7 +340,7 @@ module tb_CPU();
 
 		// SRA (addi x3, x0, -8, addi x4, x0, 1, sra x5, x3, x4)
 		test_instruction(32'hFF800193, 32'h00100213, 32'h4041D2B3, 32'h0);
-		#80
+		#220
 		if(dut.reg_file.reg_out[5] == 32'hFFFFFFFC) begin
 			 $display("SRA PASSED");
 			 pass_count = pass_count + 1;
@@ -344,7 +353,7 @@ module tb_CPU();
 
 		// BLT (addi x3, x0, 5, addi x4, x0, 10, blt x3, x4, 8, addi x5, x0, 9)
 		test_instruction(32'h00500193, 32'h00A00213, 32'h0041C463, 32'h00900293);
-		#100
+		#260
 		if(dut.reg_file.reg_out[5] != 32'd9) begin
 			 $display("BLT PASSED");
 			 pass_count = pass_count + 1;
@@ -357,7 +366,7 @@ module tb_CPU();
 
 		// BGE (addi x3, x0, 10, addi x4, x0, 5, bge x3, x4, 8, addi x5, x0, 9)
 		test_instruction(32'h00A00193, 32'h00500213, 32'h0041D463, 32'h00900293);
-		#100
+		#260
 		if(dut.reg_file.reg_out[5] != 32'd9) begin
 			 $display("BGE PASSED");
 			 pass_count = pass_count + 1;
@@ -370,7 +379,7 @@ module tb_CPU();
 
 		// BLTU (addi x3, x0, 5, addi x4, x0, 10, bltu x3, x4, 8, addi x5, x0, 9)
 		test_instruction(32'h00500193, 32'h00A00213, 32'h0041E463, 32'h00900293);
-		#100
+		#260
 		if(dut.reg_file.reg_out[5] != 32'd9) begin
 			 $display("BLTU PASSED");
 			 pass_count = pass_count + 1;
@@ -383,7 +392,7 @@ module tb_CPU();
 
 		// BGEU (addi x3, x0, 10, addi x4, x0, 5, bgeu x3, x4, 8, addi x5, x0, 9)
 		test_instruction(32'h00A00193, 32'h00500213, 32'h0041F463, 32'h00900293);
-		#100
+		#260
 		if(dut.reg_file.reg_out[5] != 32'd9) begin
 			 $display("BGEU PASSED");
 			 pass_count = pass_count + 1;
@@ -396,7 +405,7 @@ module tb_CPU();
 
 		// JAL (jal x3, 8, addi x4, x0, 9, addi x5, x0, 1)
 		test_instruction(32'h008001EF, 32'h00900213, 32'h00100293, 32'h0);
-		#100
+		#260
 		if(dut.reg_file.reg_out[3] == 32'd4 && dut.reg_file.reg_out[4] != 32'd9) begin
 			 $display("JAL PASSED");
 			 pass_count = pass_count + 1;
@@ -409,7 +418,7 @@ module tb_CPU();
 
 		// JALR (addi x3, x0, 12, jalr x4, x3, 0, addi x5, x0, 9, nop)
 		test_instruction(32'h00C00193, 32'h00018267, 32'h00900293, 32'h00000013);
-		#100
+		#260
 		if(dut.reg_file.reg_out[4] == 32'd8 && dut.reg_file.reg_out[5] != 32'd9) begin
 			  $display("JALR PASSED");
 			  pass_count = pass_count + 1;
@@ -420,9 +429,9 @@ module tb_CPU();
 
 		clear_all();
 
-		// SB (lui x3, 0x00000010, addi x4, x0, 0xAB, sb x4, 0(x3))
-		test_instruction(32'h000101B7, 32'h00018193, 32'h0AB00213, 32'h00418023);
-		#80
+		// SB (lui x3, 0x1, addi x3, x3, 0x400, addi x4, x0, 0xAB, sb x4, 0(x3))
+		test_instruction(32'h000011B7, 32'h40018193, 32'h0AB00213, 32'h00418023);
+		#220
 		if(dut.RAM.mem_array[0] == 8'hAB) begin
 			 $display("SB PASSED");
 			 pass_count = pass_count + 1;
@@ -431,12 +440,14 @@ module tb_CPU();
 			 $display("SB FAILED");
 		end
 
-		clear_all();
+   	clear_all();
 
-		// SH (lui x3, 0x00000010, li x4, 0xABCD, sh x4, 0(x3))
-		test_instruction(32'h000101B7, 32'h0000B237, 32'hBCD20213, 32'h00419023);
-		#80
-		if({dut.RAM.mem_array[1], dut.RAM.mem_array[0]} == 16'hABCD) begin
+		// RAM_BASE = 0x1400
+
+		// SH (lui x3, 0x1, li x4, 0xABCD, sh x4, 0x400(x3))
+		test_instruction(32'h000011B7, 32'h0000B237, 32'hBCD20213, 32'h40419023);
+		#350
+		if(dut.RAM.mem_array[0][15:0] == 16'hABCD) begin
 			 $display("SH PASSED");
 			 pass_count = pass_count + 1;
 		end
@@ -446,9 +457,9 @@ module tb_CPU();
 
 		clear_all();
 		
-		// LB (addi x3, x0, 24, addi x4, x0, -2, sb x4, 0(x3), lb x5, 0(x3))
-		test_instruction(32'h01800193, 32'hFFE00213, 32'h00418023, 32'h00018283);
-		#90
+		// LB (lui x3, 0x1, addi x4, x0, -2, sb x4, 0x400(x3), lb x5, 0x400(x3))
+		test_instruction(32'h000011B7, 32'hFFE00213, 32'h40418023, 32'h40018283);
+		#240
 		if(dut.reg_file.reg_out[5] == 32'hFFFFFFFE) begin
 			 $display("LB PASSED");
 			 pass_count = pass_count + 1;
@@ -459,9 +470,9 @@ module tb_CPU();
 
 		clear_all();
 
-		// LH (addi x3, x0, 28, addi x4, x0, -2, sh x4, 0(x3), lh x5, 0(x3))
-		test_instruction(32'h01C00193, 32'hFFE00213, 32'h00419023, 32'h00019283);
-		#90
+		// LH (lui x3, 0x1, addi x4, x0, -2, sh x4, 0x400(x3), lh x5, 0x400(x3))
+		test_instruction(32'h000011B7, 32'hFFE00213, 32'h40419023, 32'h40019283);
+		#240
 		if(dut.reg_file.reg_out[5] == 32'hFFFFFFFE) begin
 			 $display("LH PASSED");
 			 pass_count = pass_count + 1;
@@ -472,9 +483,9 @@ module tb_CPU();
 
 		clear_all();
 
-		// LBU (addi x3, x0, 24, addi x4, x0, -2, sb x4, 0(x3), lbu x5, 0(x3))
-		test_instruction(32'h01800193, 32'hFFE00213, 32'h00418023, 32'h0001C283);
-		#90
+		// LBU (lui x3, 0x1, addi x4, x0, -2, sb x4, 0x400(x3), lbu x5, 0x400(x3))
+		test_instruction(32'h000011B7, 32'hFFE00213, 32'h40418023, 32'h4001C283);
+		#240
 		if(dut.reg_file.reg_out[5] == 32'h000000FE) begin
 			 $display("LBU PASSED");
 			 pass_count = pass_count + 1;
@@ -485,9 +496,9 @@ module tb_CPU();
 
 		clear_all();
 
-		// LHU (addi x3, x0, 28, addi x4, x0, -2, sh x4, 0(x3), lhu x5, 0(x3))
-		test_instruction(32'h01C00193, 32'hFFE00213, 32'h00419023, 32'h0001D283);
-		#90
+		// LHU (lui x3, 0x1, addi x4, x0, -2, sh x4, 0x400(x3), lhu x5, 0x400(x3))
+		test_instruction(32'h000011B7, 32'hFFE00213, 32'h40419023, 32'h4001D283);
+		#240
 		if(dut.reg_file.reg_out[5] == 32'h0000FFFE) begin
 			 $display("LHU PASSED");
 			 pass_count = pass_count + 1;
@@ -500,7 +511,7 @@ module tb_CPU();
 
 		// AUIPC (auipc x3, 1) -> Assuming base PC=0 for test instruction, expecting 0x1000
 		test_instruction(32'h00001197, 32'h0, 32'h0, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[3] == 32'h00001000) begin
 			 $display("AUIPC PASSED");
 			 pass_count = pass_count + 1;
@@ -511,7 +522,7 @@ module tb_CPU();
 		
 		// EBREAK (addi x3, x0, 8, ebreak, addi x4, x0, 9)
 		test_instruction(32'h00800193, 32'h00100073, 32'h00900213, 32'h0);
-		#70
+		#200
 		if(dut.reg_file.reg_out[4] != 32'd9) begin
 			$display("EBREAK PASSED");
 			pass_count = pass_count + 3; //EBREAK, ECALL, FENCE
@@ -523,32 +534,33 @@ module tb_CPU();
 		clear_all();
 		
 		$display("%0d/40 INDIVIDUAL INSTRUCTIONS EXECUTED CORRECTLY", pass_count);
-		$display("TESTING SUMTEST PROGRAM...");
-		
-		$readmemh("sumtest_ROM.hex", dut.ROM.mem_array);
-		$readmemh("sumtest_RAM.hex", dut.RAM.mem_array);
-		
+		$display("Running Program...");
+
+		$readmemh("pong.hex", dut.ROM.mem_array);
+
 		rst_n = 0;
-		#20
+		#40;
 		rst_n = 1;
-		
-		#650
-		if({dut.RAM.mem_array[3], dut.RAM.mem_array[2], dut.RAM.mem_array[1], dut.RAM.mem_array[0]} == 32'h0f) begin
-			$display("SUMTEST PASSED");
-			sumtest_pass = sumtest_pass + 1;
-		end
-		else begin
-			$display("SUMTEST FAILED");
-		end
-		
-		if(pass_count == 40 && sumtest_pass == 1) begin
-			$display("CPU FUNCTIONING");
-		end
-		else begin
-			$display("CPU BUG DETECTED");
-		end
+
+		measuring    = 1'b1;
+
+		#2000000; // 2 ms
+
+		measuring = 1'b0;
+		$display("CPI = %f (%0d cycles / %0d instr)", real'(cycle_count)/real'(finish_count), cycle_count, finish_count);		
 	end
 
+	always_ff @(posedge clk) begin
+		if (!rst_n) begin
+			cycle_count  <= 0;
+			finish_count <= 0;
+		end 
+		else if (measuring) begin
+			cycle_count <= cycle_count + 1; 
+			if (!dut.MEMWB_hazard_flush && !dut.IDEX_hazard_flush && !dut.pcsrc) 
+				finish_count <= finish_count + 1; // +1 if instruction reached WB
+		end
+	end
 	
 	// Block to determine instructions if needed
 	always_comb begin
@@ -636,4 +648,3 @@ module tb_CPU();
 	end
 	
 endmodule
-	
